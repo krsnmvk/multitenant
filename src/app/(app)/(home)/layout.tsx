f324908c6055db076463caa737586ff056b4f638
Krisno Mukti <krsnmvk@gmail.com>
2025-05-13 16:41:04 +0700

    05: search filters

--- src/app/(app)/(home)/layout.tsx
+++ src/app/(app)/(home)/layout.tsx
@@ -1,11 +1,39 @@
 import { Props } from '@/utils/props';
 import Navbar from './_components/navbar';
 import Footer from './_components/footer';
+import SearchFilters from './_components/search-filters';
+import configPromise from '@payload-config';
+import { getPayload } from 'payload';
+import { Category } from '@/payload-types';
 
-export default function HomeLayout({ children }: Props) {
+export default async function HomeLayout({ children }: Props) {
+  const payload = await getPayload({
+    config: configPromise,
+  });
+
+  const data = await payload.find({
+    collection: 'categories',
+    depth: 1,
+    pagination: false,
+    where: {
+      parent: {
+        exists: false,
+      },
+    },
+  });
+
+  const formattedData = data.docs.map((doc) => ({
+    ...doc,
+    subcategories: (doc.subcategories?.docs || []).map((doc) => ({
+      ...(doc as Category),
+      subcategories: undefined,
+    })),
+  }));
+
   return (
     <div className="flex flex-col min-h-screen">
       <Navbar />
+      <SearchFilters data={formattedData} />
       <div className="flex-1 bg-[#f4f4f0]">{children}</div>
       <Footer />
     </div>
